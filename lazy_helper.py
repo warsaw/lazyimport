775672834a594d61950346d7df15c22fb1d3ecf2
Neil Schemenauer <nas@arctrix.com>
2017-09-08 07:45:18 -0700

    Add lazy_helper.py.  Used by lazy_compile.py, put in path.

---
+++ lazy_helper.py
@@ -0,0 +1,21 @@
+import sys
+import marshal
+
+# global dict holding marshal data, keyed by func name
+LAZY_DATA = '__lazy_data'
+
+def set_class(name):
+    # Give the module 'name' the __getattr__ hook needed to load
+    # functions as they are accessed.
+    #print(f'lazy setup {name}')
+    mod = sys.modules[name]
+    class Lazy(type(sys)):
+        def __getattr__(self, func):
+            #print(f'wake func {func}')
+            data = self.__dict__[LAZY_DATA].get(func)
+            if data is not None:
+                code = marshal.loads(data)
+                exec(code, self.__dict__)
+                return self.__dict__[func]
+            raise AttributeError()
+    sys.modules[name].__class__ = Lazy
